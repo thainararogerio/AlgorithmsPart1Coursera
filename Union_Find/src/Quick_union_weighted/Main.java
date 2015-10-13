39239893ac562dde9ba432c02a25d5e9504da829
Thainarar Rogério <thainararogerio@gmail.com>
2015-10-13 13:43:10 -0300

    Adding union-find-weighted algorithm

---
+++ Union_Find/src/Quick_union_weighted/Main.java
@@ -0,0 +1,24 @@
+package Quick_union_weighted;
+import Utils.StdIn;
+import Utils.StdOut;
+
+public class Main {
+	public static void main(String[] args) {
+		StdOut.println("Quick-union-weighted");
+		int N = StdIn.readInt();
+		UF uf = new UF(N);
+		
+		while (!StdIn.isEmpty()) {
+			int p = StdIn.readInt();
+			int q = StdIn.readInt();
+			
+			if (!uf.connected(p, q)) {
+				uf.union(p, q);
+				//StdOut.println(p + " " + q);
+				StdOut.println(uf);
+			}
+		}
+		
+		
+	}
+}
